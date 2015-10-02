84ac96c27a3b98070ece8c99d2430faca3178c55
adamancer <mansura@si.edu>
2015-10-02 17:41:55 -0400

    Revamped patch_mosaic to use skipped file list instead of tiles

--- mosaic.py
+++ mosaic.py
@@ -50,7 +50,7 @@ class Counter(dict):
 class Mosaic(object):
 
 
-    def __init__(self, path=None):
+    def __init__(self, path):
         # A quick list of properties of the Mosaic object:
         #  self.filename (str, specific)
         #  self.name (str, specific)
@@ -82,14 +82,15 @@ class Mosaic(object):
             self.image_types = dict([(row[0].lower(), row[1])
                                       for row in rows if bool(row[0])
                                       and not row[0].startswith('#')])
-        self.set_mosaic_parameters()
         self.get_tile_parameters(path)
+        self.set_mosaic_parameters()
 
 
 
 
     def get_tile_parameters(self, path):
         """Determine parameters for tiles in path"""
+        self.path = path
         exts = Counter()
         dims = Counter()
         tiles = []
@@ -102,12 +103,12 @@ class Mosaic(object):
             else:
                 exts.add(os.path.splitext(fn)[1])
                 dims.add('x'.join([str(x) for x in img.size]))
-                tiles.append(fp)
+                tiles.append(fp.encode('latin1').decode('latin1'))
         self.ext = [key for key in exts
                     if exts[key] == max(exts.values())][0].lower()
         dim = [key for key in dims if dims[key] == max(dims.values())][0]
         self.w, self.h = [int(x) for x in dim.split('x')]
-        self.filename = os.path.basename(path)
+        self.filename = unicode(os.path.basename(path))
         try:
             self.name, image_type = self.filename.rsplit('_', 1)
             self.name += ' ({})'.format(self.image_types[image_type.lower()])
@@ -119,6 +120,7 @@ class Mosaic(object):
             pass
         tiles = [tile for tile in tiles if tile.endswith(self.ext)]
         self.handle_tiles(tiles)
+        return self
 
 
 
@@ -135,7 +137,7 @@ class Mosaic(object):
                    ' filenames (n={})').format(self.num_cols)
         self.mag = int(prompt('Magnification:', '\d+'))
         self.snake = prompt('Snake pattern?', yes_no)
-        self.rows = self.mandolin(self.tiles, self.num_cols)
+        self.rows = mandolin(self.tiles, self.num_cols)
         self.num_rows = len(self.rows)
         self.determine_offset()
         if prompt('Are these parameters okay?', yes_no):
@@ -155,7 +157,7 @@ class Mosaic(object):
         # bunch of mosaics to be run at once with the same settings.
         if path:
             self.get_tile_parameters(path)
-            self.rows = self.mandolin(self.tiles, self.num_cols)
+            self.rows = mandolin(self.tiles, self.num_cols)
             self.num_rows = len(self.rows)
         # The dimensions of the mosaic are determined by the
         # tile dimensions minus the offsets between rows and
@@ -196,7 +198,8 @@ class Mosaic(object):
                          self.y_offset_within_row * n_col)
                     if self.y_offset_within_row < 0:
                         y -= self.y_offset_within_row * (self.num_cols - 1)
-                    mosaic.paste(Image.open(fp), (x, y))
+                    # Encode the name
+                    mosaic.paste(Image.open(fp.encode('cp1252')), (x, y))
                 n_col += 1
             n_row += 1
         # Add label
@@ -210,14 +213,15 @@ class Mosaic(object):
             # Resize text to a reasonable size based on the
             # dimensions of the mosaic
             size = 100
-            font = ImageFont.truetype('Microsoft Sans Serif.ttf', size)
+            font = ImageFont.truetype('arial.ttf', size)
             w, h = font.getsize(text)
             size = int(0.8 * size * label_height / float(h))
-            font = ImageFont.truetype('Microsoft Sans Serif.ttf', size)
+            font = ImageFont.truetype('arial.ttf', size)
             x = int(0.02 * mosaic_width)
             y = mosaic_height - int(label_height)
             draw.text((x, y), text, (0, 0, 0), font=font)
-        mosaic.save(self.filename + '.tif', self.extmap[self.ext])
+        fp = os.path.join(self.path, os.pardir, self.filename + '.tif')
+        mosaic.save(fp, self.extmap[self.ext])
         print 'Stitching complete! (t={})'.format(datetime.now() - start_time)
         # Clear folder-specific parameters. These will be repopulated
         # automatically if the same Mosaic object is used for a
@@ -280,7 +284,7 @@ class Mosaic(object):
                 # coordinates in the grid to calculate the number
                 # of columns.
                 x, y = key.split(' ')
-                cols.append(y)
+                cols.append(int(x))
                 i = key
             except ValueError:
                 i = key
@@ -289,19 +293,24 @@ class Mosaic(object):
             self.num_cols = max(cols) + 1
             for key in temp.keys():
                 x, y = key.split(' ')
-                i = int(y) * self.num_cols + int(x)
-                temp[i] = tile
+                i = int(x) + self.num_cols * int(y)
+                temp[i] = temp[key]
                 del temp[key]
+        # Create tiles and rows
+        tiles = [temp[key] for key in sorted(temp.keys())]
+        tiles = self.patch_tiles(tiles)
         # Check the tileset for coherence
         keys = temp.keys()
         idealized_keys = set([x for x in xrange(min(keys), max(keys)+1)])
         missing = idealized_keys - set(keys)
-        if len(missing):
+        empties = [tile for tile in tiles if not bool(tile)]
+        if len(missing) and not len(empties):
             print 'Warning: The following tiles appear to be missing:'
             for key in sorted(missing):
-                print ' {}'.format(temp[key])
-        # Create tiles and rows
-        self.tiles = [temp[key] for key in sorted(temp.keys())]
+                print ' Index: {}'.format(key)
+        else:
+            print 'All tiles appear to be present'
+        self.tiles = tiles
 
 
 
@@ -319,71 +328,52 @@ class Mosaic(object):
         #   tile is to the RIGHT of the left edge of the upper tile.
         #   Because tiles must be shifted up to overlap, y is
         #   always negative.
-        self.x_offset_within_row = -100  # must be negative
-        self.x_offset_between_rows = -50
-        self.y_offset_within_row = -25
-        self.y_offset_between_rows = -124  # must be negative
+        self.x_offset_within_row = -190  # must <= 0
+        self.x_offset_between_rows = -6
+        self.y_offset_within_row = -4
+        self.y_offset_between_rows = -56  # must <= 0
         return self
 
 
 
 
-    def mandolin(self, lst, n):
-        """Split list into groups of n members
-
-        @param list
-        @param int
-        @return list
-        """
-        return [lst[i*n:(i+1)*n] for i in range(len(lst) / n)]
-
-
-
-
     def patch_tiles(self, tiles):
-        """Patch tileset with blanks based on mosaic.Selector"""
         """Returns sorted list of tiles including patches"""
-        patches = glob.glob(os.path.join(path, '..', 'output',
-                                         'placeholders', '*' + ext))
-        if len(patches):
-            print 'Patching mosaic with tiles set aside by selector.py...'
-        # Insert patches into sequence
+        try:
+            f = open(os.path.join(self.path, 'skipped.txt'), 'rb')
+        except:
+            return tiles
         sequence = {}
-        for tile in patches:
-            coords = os.path.splitext(tile)[0].split('@')[1].rstrip(']').split(' ')
-            y, x = [int(x) for x in coords]
-            i = x * num_cols + y
-            sequence[i] = tile
-        tiles = glob.glob(os.path.join(path, '*' + ext))
-        tiles.sort(key=lambda fn: sorter(fn))
-        j = 0
+        skipped = [int(i.strip()) for i in f.read().splitlines()]
+        for i in skipped:
+            sequence[i] = ''
+        i = 0
         for tile in tiles:
             while True:
                 try:
-                    sequence[j]
+                    sequence[i]
                 except:
-                    sequence[j] = tile
+                    sequence[i] = tile
                     break
                 else:
-                    j += 1
+                    i += 1
         tiles = []
         for i in sorted(sequence.keys()):
             tiles.append(sequence[i])
-        #print '\n'.join(tiles)
+        print 'Tile set was patched!'
         return tiles
 
 
 
+
 def mosey(path=None):
-    """Helper function"""
+    """Helper function for stitching a set of directories all at once"""
     if not path:
         root = Tkinter.Tk()
         root.withdraw()
-        title = ("Please select the directory containg the tilesets"
-                 " you'd like to stitch.")
+        title = "Please select the directory containing your tile sets:"
         initial = os.path.expanduser('~')
-        path = tkFileDialog.askdirectory(parent=root,
-                                         title=title,
+        path = tkFileDialog.askdirectory(parent=root, title=title,
                                          initialdir=initial)
     print path
     for path in ([os.path.join(path, dn) for dn in os.listdir(path)
@@ -393,3 +383,15 @@ def mosey(path=None):
             mosaic.create_mosaic(path)
         except NameError:
             mosaic = Mosaic(path).create_mosaic()
+
+
+
+
+def mandolin(lst, n):
+    """Split list into groups of n members
+
+    @param list
+    @param int
+    @return list
+    """
+    return [lst[i*n:(i+1)*n] for i in range(len(lst) / n)]
