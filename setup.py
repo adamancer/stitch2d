74fb4b1cb24e5ba7183e5ce69537c93752e4c57f
adamancer <mansura@si.edu>
2015-10-30 13:05:03 -0400

    Changed version to 0.23

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except:
                         ' for more information about Stitch2D.')
 
 setup(name='stitch2d',
-      version='0.22',
+      version='0.23',
       description='Stitch a planar set of tiles into a mosaic',
       long_description=long_description,
       classifiers = [
